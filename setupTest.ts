a8ced30dc7b24a5f8648a9325768195fa3d13ea8
Wojciech Sierotowicz <wojexe@gmail.com>
2024-01-25 18:01:45 +0100

    Fix Vitest setup

--- setupTest.ts
+++ setupTest.ts
@@ -1,7 +1,1 @@
-// setupTest.ts
-/* eslint-disable @typescript-eslint/no-empty-function */
-import matchers from "@testing-library/jest-dom/matchers";
-import { expect } from "vitest";
-
-// Add custom jest matchers
-expect.extend(matchers);
+import '@testing-library/jest-dom/vitest'
